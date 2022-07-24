37ebbd2da70ed938b20913c3fc86a16619976559
Farshid Karimi <farshidk49@gmail.com>
2022-07-24 20:31:45 +0430

    hero created

--- src/components/future/Future.jsx
+++ src/components/future/Future.jsx
@@ -12,10 +12,56 @@ const Future = () => {
         <a href="#">Request Early Access to Get Started</a>
       </div>
 
-      <div className="future_items">
+      <div className="future-items">
+        <div className="future-item">
+          <div>
+            <hr />
+            <h3>Improving end distrusts instantly </h3>
+          </div>
 
+          <p>
+            From they fine john he give of rich he. They age and draw mrs like.
+            Improving end distrusts may instantly was household applauded.
+          </p>
+        </div>
+
+        <div className="future-item">
+          <div>
+            <hr />
+            <h3>Become the tended active</h3>
+          </div>
+
+          <p>
+            Considered sympathize ten uncommonly occasional assistance
+            sufficient not. Letter of on become he tended active enable to.
+          </p>
+        </div>
+
+        <div className="future-item">
+          <div>
+            <hr />
+            <h3>Message or am nothing</h3>
+          </div>
+
+          <p>
+            Led ask possible mistress relation elegance eat likewise debating.
+            By message or am nothing amongst chiefly address.
+          </p>
+        </div>
+
+        <div className="future-item">
+          <div>
+            <hr />
+            <h3>Really boy law county</h3>
+          </div>
+
+          <p>
+            Really boy law county she unable her sister. Feet you off its like
+            like six. Among sex are leave law built now. In built table in an
+            rapid blush.
+          </p>
+        </div>
       </div>
-
     </div>
   );
 };
